953fd8debac6d767bb50e834ab388d90325487ea
Christiaan Lombard <christiaan.lombard@yousemble.com>
2016-08-03 19:59:28 +0200

    ..

--- src.js
+++ src.js
@@ -7,16 +7,15 @@ var context = $canvas.getContext('2d');
 
 
 
-var PARTICLE_MASS = 0.001; // kg
-var PARTICLE_COUNT = 15000;
+var PARTICLE_COUNT = 12000;
 
-var MASSIVE_MASS = 1.0; // kg
-var GAMMA = 0.00000001;
+//var GAMMA = 66.73 * Math.pow(10,-12);
+var GAMMA = 0.00005;
 
-var TERMINAL_VELOCITY = 200;
+var TERMINAL_VELOCITY = 0.001;
 
-var WIDTH = 1920;
-var HEIGHT = 1080;
+var WIDTH = 800;
+var HEIGHT = 800;
 
 var lastPulseTime = (new Date).getTime();
 
@@ -27,24 +26,21 @@ var lastPulseTime = (new Date).getTime();
 // massive
 // [position_x, position_y]
 //
-//  - Particles are low mass objects moving within a frictionless world
-//  - Massives are extremely high mass and is not affected by gravity
-//  - Particles experience a gravitational pull towards Massives
-//      - F = ( G * M * m ) / r^2
-//      - F = m * a
-//
-//      - a = (G * M) / r^2
 //
 //
 
 var particles = [];
 var massives = [];
 
-function Particle(pos_x, pos_y){
+function Particle(pos_x, pos_y, color){
   this.pos_x = pos_x;
   this.pos_y = pos_y;
   this.vel_x = 0;
   this.vel_y = 0;
+  this.weight = 1000;
+  // absolute velocity
+  this.vel = 0;
+  this.color = color;
 }
 
 function Massive(pos_x, pos_y, weight){
@@ -59,59 +55,57 @@ function pulse(){
 
   // update particles
 
-  // delta t in milliseconds
-  var sinceLastPulse = (new Date).getTime() - lastPulseTime;
+  // delta t in seconds
+  var t = ((new Date).getTime() - lastPulseTime);
 
   lastPulseTime = (new Date).getTime();
 
-  for(var i = 0; i < particles.length; i++){
+  var dx, dy, r, nx, ny, a, v, G_m;
 
-    // each massive
-    for(var j = 0; j < massives.length; j++){
 
-      var dx = massives[j].pos_x - particles[i].pos_x;
-      var dy = massives[j].pos_y - particles[i].pos_y;
+  // each massive
+  for(var j = 0; j < massives.length; j++){
 
-      var r = Math.sqrt(dx * dx + dy * dy);
+    G_m = GAMMA * massives[j].weight;
 
-      //if(r < 1000) r = 1000;
-      //if(r > 1000) r = 1000;
+    for(var i = 0; i < particles.length; i++){
 
-      var ax = Math.min(dx / Math.pow(r, 3), 20);
-      var ay = Math.min(dy / Math.pow(r, 3), 20);
-
-      particles[i].vel_x += (ax * sinceLastPulse);
-      particles[i].vel_y += (ay * sinceLastPulse);
-
-      // // delta distance between particles
-      // var d_x = particles[i].pos_x - massives[j].pos_x;
-      // var d_y = particles[i].pos_y - massives[j].pos_y;
+      //  - Particles are low mass objects moving within frictionless space
+      //  - Massives are extremely high mass and is not affected by gravity
+      //  - Particles experience a gravitational pull towards Massives
+      //      - F = ( G * M * m ) / r^2
+      //      - F = m * a
       //
-      // // culculate once
-      // var G_M = 1000000; //GAMMA * massives[j].weight;
-      //
-      // // acceleration x and y for current particle
-      // var a_x = G_M / Math.pow(d_x, 2);
-      // var a_y = G_M / Math.pow(d_y, 2);
-      //
-      // particles[i].vel_x += a_x * sinceLastPulse;
-      // particles[i].vel_y += a_y * sinceLastPulse;
-      //
-      // if(particles[i].vel_x > TERMINAL_VELOCITY) particles[i].vel_x = TERMINAL_VELOCITY;
-      // if(particles[i].vel_y > TERMINAL_VELOCITY) particles[i].vel_y = TERMINAL_VELOCITY;
-      //
-      // particles[i].pos_x += (particles[i].pos_x < massives[j].pos_x) ? (particles[i].vel_x * sinceLastPulse) : (-1 * particles[i].vel_x * sinceLastPulse);
-      // particles[i].pos_y += (particles[i].pos_y < massives[j].pos_y) ? (particles[i].vel_y * sinceLastPulse) : (-1 * particles[i].vel_y * sinceLastPulse);
+      //      - a = (G * M) / r^2
+
+      dx = massives[j].pos_x - particles[i].pos_x;
+      dy = massives[j].pos_y - particles[i].pos_y;
+
+      // absolute length between massive and particle
+      r = Math.sqrt(dx * dx + dy * dy);
+
+      //r = Math.max(1, r);
+
+      // normalized vector
+      nx = dx / r;
+      ny = dy / r;
+
+      a = G_m / (r * r);
+
+      particles[i].vel = Math.min(a * t, TERMINAL_VELOCITY);
+
+      // var ax = nx * a;
+      // var ay = ny * a;
+
+      //particles[i].vel = v;
+      particles[i].vel_x += nx * particles[i].vel;
+      particles[i].vel_y += ny * particles[i].vel;
+      particles[i].pos_x += (particles[i].vel_x * t);
+      particles[i].pos_y += (particles[i].vel_y * t);
     }
-    particles[i].pos_x += (particles[i].vel_x * sinceLastPulse);
-    particles[i].pos_y += (particles[i].vel_y * sinceLastPulse);
 
+    //console.log(particles[i].vel);
   }
-
-  //console.log(sinceLastPulse);
-  // console.log('dis', d_x, d_y);
-  // console.log('vel', particles[0].vel_x, particles[0].vel_x);
-  // console.log('pos', particles[0].pos_x, particles[0].pos_y);
 
 }
 
@@ -121,30 +115,52 @@ function setup(){
   $canvas.height = HEIGHT;
 
   for(var i = 0; i < PARTICLE_COUNT; i++){
-    particles.push(new Particle(Math.random()*WIDTH,Math.random()*HEIGHT));
+    particles.push(new Particle(Math.random()*WIDTH,Math.random()*HEIGHT, 'rgb(' + [Math.round(Math.random() * 250), Math.round(Math.random() * 250), Math.round(Math.random() * 250)].join(',') + ')'));
   }
 
-  massives.push(new Massive(Math.random()*WIDTH,Math.random()*HEIGHT,10));
-  massives.push(new Massive(Math.random()*WIDTH,Math.random()*HEIGHT,10));
+  massives.push(new Massive(Math.random()*WIDTH,Math.random()*HEIGHT,Math.random() * 1000000));
+  massives.push(new Massive(Math.random()*WIDTH,Math.random()*HEIGHT,Math.random() * 1000000));
+  massives.push(new Massive(Math.random()*WIDTH,Math.random()*HEIGHT,Math.random() * 1000000));
+  //massives.push(new Massive(Math.random()*WIDTH,Math.random()*HEIGHT,Math.random() * 1000000));
 
+  window.setInterval(draw, 33);
 
 }
 
 function draw(){
-  requestAnimationFrame(draw);
+  //requestAnimationFrame(draw);
   //console.debug('draw');
-  context.clearRect(0, 0, WIDTH, HEIGHT);
-
   pulse();
 
-  console.log(Math.round(Math.sqrt(particles[0].vel_x * particles[0].vel_x + particles[0].vel_y * particles[0].vel_y)));
+  context.clearRect(0, 0, WIDTH, HEIGHT);
 
-  //draw points
+
+  //console.log(Math.round(Math.sqrt(particles[0].vel_x * particles[0].vel_x + particles[0].vel_y * particles[0].vel_y)));
+
+  context.fillStyle = '#222222';
+
+  // draw massives
+  for(i = 0; i < massives.length; i++){
+    //draw a circle
+    if(massives[i].pos_x > 0 && massives[i].pos_y > 0 && massives[i].pos_x < WIDTH && massives[i].pos_y < HEIGHT){
+      context.beginPath();
+      context.arc(massives[i].pos_x, massives[i].pos_y, 10, 0, Math.PI*2, true);
+      context.closePath();
+      context.fill();
+    }
+  }
+
+  context.fillStyle = '#ffffff'; //toColor(Math.round(Math.sqrt(particles[i].vel_x * particles[i].vel_x + particles[i].vel_y * particles[i].vel_y) * 5000000));
+
+  //draw particles
   for(var i = 0; i < particles.length; i++){
       //draw a circle
       if(particles[i].pos_x > 0 && particles[i].pos_y > 0 && particles[i].pos_x < WIDTH && particles[i].pos_y < HEIGHT){
-        context.fillStyle = '#ffffff';//toColor(Math.round(Math.sqrt(particles[i].vel_x * particles[i].vel_x + particles[i].vel_y * particles[i].vel_y) * 5000000));
-        context.fillRect(particles[i].pos_x, particles[i].pos_y, 2, 2);
+
+        context.fillStyle = particles[i].color;
+        var size = 1 / ((particles[i].vel + 0.01) * 60)  ;
+
+        context.fillRect(particles[i].pos_x, particles[i].pos_y, size, size);
         // context.beginPath();
         // context.arc(Math.round(particles[i].pos_x), Math.round(particles[i].pos_y), 1, 0, Math.PI*2, true);
         // context.closePath();
@@ -152,18 +168,6 @@ function draw(){
       }
   }
 
-  context.fillStyle = '#222222';
-
-  // draw massives
-  for(i = 0; i < massives.length; i++){
-      //draw a circle
-      if(massives[i].pos_x > 0 && massives[i].pos_y > 0 && massives[i].pos_x < WIDTH && massives[i].pos_y < HEIGHT){
-        context.beginPath();
-        context.arc(massives[i].pos_x, massives[i].pos_y, 10, 0, Math.PI*2, true);
-        context.closePath();
-        context.fill();
-      }
-  }
 
 }
 
@@ -177,4 +181,4 @@ function toColor(num) {
 
 setup();
 
-draw();
+//draw();
---
+++ src2.js
@@ -0,0 +1,5 @@
+console.log('Init source');
+
+var $canvas = document.getElementById('particle_canvas');
+
+var context = $canvas.getContext('2d');
