000f209f56b6d6906a14cf495cd31dcecf39252e
Christiaan Lombard <base1.christiaan@gmail.com>
2020-02-03 11:31:42 +0200

    mandelbrot++

--- mandelbrot.js
+++ mandelbrot.js
@@ -1,10 +1,13 @@
 (function() {
     // Create Canvas
+    const WIDTH = 2000;
+    const HEIGHT = 2000;
     var myCanvas = document.createElement("canvas");
-    myCanvas.width=900;
-    myCanvas.height=900;
+    myCanvas.width=WIDTH;
+    myCanvas.height=HEIGHT;
     document.body.appendChild(myCanvas);
     var ctx = myCanvas.getContext("2d");
+    let image = ctx.createImageData(WIDTH,HEIGHT);
 
     function checkIfBelongsToMandelbrotSet(x,y) {
         var realComponentOfResult = x;
@@ -29,22 +32,32 @@
     // Start drawing
     var magnificationFactor = 8000;
     var panX = 0.6;
-    var panY = 0.6;
-    for(var x=0; x < myCanvas.width; x++) {
-       for(var y=0; y < myCanvas.height; y++) {
+    var panY = 0.65;
+    ctx.fillStyle = '#000';
+    ctx.fillRect(0,0,WIDTH, HEIGHT);
+    ctx.fillStyle = '#fff';
+    for(var x=0; x < WIDTH; x++) {
+       for(var y=0; y < HEIGHT; y++) {
            var belongsToSet = 
                 checkIfBelongsToMandelbrotSet(x/magnificationFactor - panX, 
                                               y/magnificationFactor - panY);
+
             if(belongsToSet == 0) {
-                ctx.fillStyle = '#000';
-                ctx.fillRect(x,y, 1,1); // Draw a black pixel
+                image.data[(x * 4) + (y * WIDTH * 4)] = 0;
+                image.data[(x * 4) + (y * WIDTH * 4) + 1] = 0;
+                image.data[(x * 4) + (y * WIDTH * 4) + 2] = 0;
+                image.data[(x * 4) + (y * WIDTH * 4) + 3] = 2555;
             } else {
-                ctx.fillStyle = 'hsl(0,100%,' + belongsToSet + '%)';
-                ctx.fillRect(x,y, 1,1); // Draw a colorful pixel
+                image.data[(x * 4) + (y * WIDTH * 4)] = belongsToSet / 100 * 255;
+                image.data[(x * 4) + (y * WIDTH * 4) + 1] = belongsToSet;
+                image.data[(x * 4) + (y * WIDTH * 4) + 2] = 0;
+                image.data[(x * 4) + (y * WIDTH * 4) + 3] = 255;
             }           
        } 
+
+       ctx.fillRect(0,0,x/WIDTH, HEIGHT);
     }
 
-    console.log('done');
+    ctx.putImageData(image, 0, 0);
 
 })();
